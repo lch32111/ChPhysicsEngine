73f0cc67b2b9c063ba17800d14890f6e2605c5d1
Chanhaeng Lee <lch32111@naver.com>
2019-01-08 16:48:59 +0900

    added DirectX11 practice

---
+++ ChanPhysicsPlayGround/chContactSolver.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifndef __CH_CONTACT_SOLVER_H__
+#define __CH_CONTACT_SOLVER_H__
+
+
+#endif
---
+++ Dx11Practice/SwapChain.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <d3d11.h>
+
+class SwapChain
+{
+public:
+	SwapChain();
+	bool init(HWND hwnd, UINT width, UINT height);
+	bool release();
+	~SwapChain();
+
+private:
+	IDXGISwapChain* m_swap_chain;
+};
+
